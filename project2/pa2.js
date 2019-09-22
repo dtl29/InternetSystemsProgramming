16a63614d17fca551b9c37e53aee28e5b66a4a91
dtl29 <44950643+dtl29@users.noreply.github.com>
2019-09-22 12:46:29 -0400

    Add files via upload

--- project2/pa2.js
+++ project2/pa2.js
@@ -4,47 +4,66 @@ Internet Systems Programming Fall 2019
 Project 2
 */
 
-function calculator()
-{
-}
-
 function animation()
 {
-	
+	var anim = document.getElementById("anim");
 }
 
-function emailCheckWhileTypeing()
+function emailCheckWhileTyping()
 {
 	var em = document.getElementById("email").value;
-    
-	document.getElementById("inputEmail").innerHTML = "you're typomg";
-}
-function emailCheckWhileClicked()
-{
-    var em = document.getElementById("emailval").value;
-       document.getElementById("inputEmail").innerHTML = "clicked me did'ya "+em;
-    var str1 = em;
-    var str2 = em;
-    var str3 = em;
-    var pnt = em.search("@");
-    var pnt2 = em.search(".");
-    
-    str1 = str1.slice(pnt);
-    str2 = str2.substr(pnt, pnt2);
-    str3 = str3.substr(pnt2, str3.length());
-    
-    document.getElementById("inputEmail").innerHTML = "clicked me did'ya "+em+" " +st1;
-    
-    if(str1.length() > 0 && str2.length() > 0 && str3.length() > 0)
-    {
-        document.getElementById("inputEmail").innerHTML = "This is a valide email.";
-    }
-    else
-    {
-        document.getElementById("inputEmail").innerHTML = "This is an invalide email.";
-    }
+    	var pos = em.search("@");
+	var str1 = em.substr(0,pos);
+	var str2 = em.slice(pos+1);
+	var pos2 = str2.indexOf(".");
+	var str3 = str2.slice(pos2+1);
+	var str2 = str2.substr(0, pos2);
+		
+	if(str1.match(/^[0-9a-zA-Z]+$/) == null || str1.length < 0)
+	{
+		document.getElementById("inputEmail").innerHTML = "<p>Invalid <br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' </p>";	
+	}
+	else if(str2.match(/^[0-9a-zA-Z]+$/) == null || str2.length < 0)
+	{
+		document.getElementById("inputEmail").innerHTML = "<p>Invalid <br/>Please have no extra special characters after the '@'.<br/>And needs to contain 1 '@' symbol and 1 '.'  </p>";
+	}
+	else if((str3.length > 3) || str3.length < 0 || (str3.match(/^[0-9a-zA-Z]+$/) == null))
+	{
+		document.getElementById("inputEmail").innerHTML = "<p>Invalid<br/>The ending should be no more than 3 charaters long.<br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' </p>";
+	}
+	else
+	{
+		document.getElementById("inputEmail").innerHTML = "<p>The entered email is valid</p>";
+	}
 }
 
+function emailCheck()
+{
+	var em = document.getElementById("emailval").value;
+    	var pos = em.search("@");
+	var str1 = em.substr(0,pos);
+	var str2 = em.slice(pos+1);
+	var pos2 = str2.indexOf(".");
+	var str3 = str2.slice(pos2+1);
+	var str2 = str2.substr(0, pos2);
+		
+	if(str1.match(/^[0-9a-zA-Z]+$/) == null || str1.length < 0)
+	{
+		document.getElementById("inputEmail2").innerHTML = "<p>Invalid <br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' </p>";	
+	}
+	else if(str2.match(/^[0-9a-zA-Z]+$/) == null || str2.length < 0)
+	{
+		document.getElementById("inputEmail2").innerHTML = "<p>Invalid <br/>Please have no extra special characters after the '@'.<br/>And needs to contain 1 '@' symbol and 1 '.'  </p>";
+	}
+	else if((str3.length > 3) || str3.length < 0 || (str3.match(/^[0-9a-zA-Z]+$/) == null))
+	{
+		document.getElementById("inputEmail2").innerHTML = "<p>Invalid<br/>The ending should be no more than 3 charaters long.<br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' </p>";
+	}
+	else
+	{
+		document.getElementById("inputEmail2").innerHTML = "<p>The entered email is valid</p>";
+	}
+}
 function factorialCalc()
 {
 	var num = document.getElementById("number").value;
