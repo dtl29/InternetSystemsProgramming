1f78169d2dc141ee72e450ab8e54d1b205b1e28e
dtl29 <44950643+dtl29@users.noreply.github.com>
2019-09-23 16:54:39 -0400

    Add files via upload

--- project2/pa2.js
+++ project2/pa2.js
@@ -3,7 +3,11 @@ Daniel Louis
 Internet Systems Programming Fall 2019
 Project 2
 */
-
+function time()
+{
+	var d = new Date();
+	document.getElementById("time").innerHTML += d;
+}
 function animation(event)
 {
 	var anim = document.getElementById("anim");
@@ -44,19 +48,19 @@ function emailCheckWhileTyping()
 		
 	if(str1.match(/^[0-9a-zA-Z]+$/) == null || str1.length < 0)
 	{
-		document.getElementById("inputEmail").innerHTML = "<p>Invalid <br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' </p>";	
+		document.getElementById("inputEmail").innerHTML = "<p>Invalid <br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' <br/><br/></p>";	
 	}
 	else if(str2.match(/^[0-9a-zA-Z]+$/) == null || str2.length < 0)
 	{
-		document.getElementById("inputEmail").innerHTML = "<p>Invalid <br/>Please have no extra special characters after the '@'.<br/>And needs to contain 1 '@' symbol and 1 '.'  </p>";
+		document.getElementById("inputEmail").innerHTML = "<p>Invalid <br/>Please have no extra special characters after the '@'.<br/>And needs to contain 1 '@' symbol and 1 '.'  <br/><br/></p>";
 	}
 	else if((str3.length > 3) || str3.length < 0 || (str3.match(/^[0-9a-zA-Z]+$/) == null))
 	{
-		document.getElementById("inputEmail").innerHTML = "<p>Invalid<br/>The ending should be no more than 3 charaters long.<br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' </p>";
+		document.getElementById("inputEmail").innerHTML = "<p>Invalid<br/>The ending should be no more than 3 charaters long.<br/>Please have no extra special characters.<br/>And needs to contain 1 '@' symbol and 1 '.' <br/><br/></p>";
 	}
 	else
 	{
-		document.getElementById("inputEmail").innerHTML = "<p>The entered email is valid</p>";
+		document.getElementById("inputEmail").innerHTML = "<p>The entered email is valid<br/><br/></p>";
 	}
 }
 
