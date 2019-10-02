327efafd18d4cf7eb4f9135943cdbc8d1bdb636a
dtl29 <44950643+dtl29@users.noreply.github.com>
2019-10-02 17:08:45 -0400

    Add files via upload

--- project3/pa3.php
+++ project3/pa3.php
@@ -17,58 +17,47 @@ Project 3
 
 	//get the inputs from the html 
 	$operand = $_POST["button"];
-	/*
-	//curent has no value and need some
-	$curentValue = $_POST["current"];
-	$newValue = $_POST["input"];
+	$curentValue = $_POST["curent"];
+	$newValue = $_POST["new"];
 
 	//convert the strings into numbers
 	if($newValue == ""){$newValue = 0.0;}
 	else if(!(is_numeric($newValue))){$newValue = 0.0;}
 	else {$newValue = (float)$newValue;}
-	if($curentValue == ""){$curentValue = 0.0;}
-	else
-	{
-		$strLength = strlen($curentValue);
-		//its either 15 or 16 depending on if the string starts with 
-		//0 or not
-		$curentValue = substr($curentValue,15,$strLength)
-		$curentValue = (float)$curentValue;
-	}
-	if($newValue = ""){$newValue = "=";}
+	$curentValue = (float)$curentValue;
 
 	//perform the actions
 	if($operand == "+")
 	{
-		$curentValue += $newValue;
+		$curentValue = $curentValue + $newValue;
 	}
 	else if($operand == "-")
 	{
-		$curentValue -= $newValue;
+		$curentValue = $curentValue - $newValue;
 	}
 	else if($operand == "/")
 	{
-		$curentValue /= $newValue;
+		$curentValue = $curentValue / $newValue;
 	}
 	else if($operand == "*")
 	{
-		$curentValue *= $newValue;
+		$curentValue = $curentValue * $newValue;
 	}
 	else
 	{
 		$curentValue = $newValue;
-	}*/
+	}
 
 ?>
 
-	<form action = "http://pausch.cs.uakron.edu/~dtl29/php/pa3.php" 
-		method = "post">
+<form action = "http://pausch.cs.uakron.edu/~dtl29/php/pa3.php"	method = "post">
 	<head>Daniel Louis <br/>Project 3</head>
 	<body>
 		<dir></dir>
 		<div>
-			<div name = "current" style = "margin-left : 0px;">Current value = <?php print("3"); ?></div>
-			<input type = "text" name = "input"></input><br/>
+			<div style = "margin-left : 0px;">Current Value: <?php print("$curentValue"); ?></div>
+			<input type = "hidden" name = "curent" value = "<?php print($curentValue); ?>">
+			<input type = "text" name = "new"></input><br/>
 			<table>
 				<tr><input type = "submit" name = "button" value = "+"></tr>
 				<tr><input type = "submit" name = "button" value = "-"></tr>
