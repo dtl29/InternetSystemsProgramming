9ccb7fb4d31d7096c1001b96873f41d1c7efb5ae
dtl29 <44950643+dtl29@users.noreply.github.com>
2019-10-02 13:53:41 -0400

    Add files via upload

--- project3/pa3.php
+++ project3/pa3.php
@@ -1,57 +1,65 @@
-<!-- Daniel Louis -->
+<!
+Daniel Louis
+Internet System Programming - Fall 2019
+Project 3
+>
 <!DOCTYPE html>
+<html>
 <?php
-    //get data from forms
-    $operand = $_POST["submit"];
-    $curValue = $_POST["curentValue"];
-    $newValue = $_POST["newValue"];
+	//get the inputs from the html 
+	$operand = $_POST["button"];
+	$curentValue = $_POST["input"];
+	$newValue = $_POST["current"];
 
-    //convert the strings into numbers
-    if($newValue == "") {$newValue = 0.0;}
-    else if(!(is_numeric($newValue))) {$newValue = 0.0;}
-    else {$newValue = (float)$newValue;}
-    $curValue = (float)$curValue;
+	//convert the strings into numbers
+	if($newValue == ""){$newValue = 0.0;}
+	else if(!(is_numeric($newValue))){$newValue = 0.0;}
+	else {$newValue = (float)$newValue;}
+	$strLength = strlen($curentValue);
+	//its either 15 or 16 depending on if the string starts with 
+	//0 or not
+	$curentValue = substr($curentValue,15,$strLength)
+	$curentValue = (float)$curentValue;
 
-    //perform the action
-    if($operand == "+") 
-    {
-        $curValue = $curValue + $newValue;
-    }
-    else if($operand == "-") 
-    {
-        $curValue = $curValue - $newValue;
-    }
-    else if($operand == "*") 
-    {
-        $curValue = $curValue * $newValue;
-    }
-    else if($operand == "/" && $newValue != 0.0) 
-    {
-        $curValue = $curValue / $newValue;
-    }
-    else if($operand == "=") 
-    {
-        $curValue = $newValue;
-    }
-    
-
+	//perform the actions
+	if($operand == "+")
+	{
+		$curentValue += $newValue;
+	}
+	else if($operand == "-")
+	{
+		$curentValue -= $newValue;
+	}
+	else if($operand == "/")
+	{
+		$curentValue /= $newValue;
+	}
+	else if($operand == "*")
+	{
+		$curentValue *= $newValue;
+	}
+	else
+	{
+		$curentValue = $newValue;
+	}
 ?>
-<form action = "http://pausch.cs.uakron.edu/~dtl29/pa3/pa3.php"
-          method = "post">
-<header>
-    <h2>
-        Online Calculator
-    </h2>
-</header>
-<body>
-Output <input type = "text" name = "curentValue" value = "<?php print($curValue); ?>" 
-    readonly><br/>
-        Input &nbsp<input type = "text" name = "newValue">
-	<br/><br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-	<input type = "submit" name = "submit" value = "+">
-	<input type = "submit" name = "submit" value = "-">
-	<input type = "submit" name = "submit" value = "*">
-	<input type = "submit" name = "submit" value = "/">
-	<input type = "submit" name = "submit" value = "=">
-</body>
-</form>	
+	<form action = "http://pausch.cs.uakron.edu/~dtl29/pa3/pa3.php" 
+		method = "post">
+	<head>Daniel Louis <br/>Project 3</head>
+	<body>
+		<dir></dir>
+		<div>
+			<div name = "current" style = "margin-left : 0px;">Current value = <?php print($curentValue);?></div>
+			<input type = "text" name = "input"></input><br/>
+			<table>
+				<tr><input type = "submit" name = "button" value = "+"></tr>
+				<tr><input type = "submit" name = "button" value = "-"></tr>
+				<tr><input type = "submit" name = "button" value = "*"></tr>
+				<tr><input type = "submit" name = "button" value = "/"></tr>
+				<tr><input type = "submit" name = "button" value = "="></tr>
+			</table>
+		</div>
+		<div></div>
+	</body>
+</html>
+
