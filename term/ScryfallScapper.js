4e065434226b64b86562c38f3f6a47466bb890d7
dtl29 <44950643+dtl29@users.noreply.github.com>
2019-11-04 18:38:31 -0500

    Add files via upload

--- term/ScryfallScapper.js
+++ term/ScryfallScapper.js
@@ -7,15 +7,44 @@ Term project: Scryfall scrapper
 function scrap(searchText)
 {
 
-    fetch('https://api.scryfall.com/cards/search?order=cmc&q=' + searchText)
-    .then(res => res.json())
-    .then(json => {
-        let searchResults = json.data;
-        let output='<div>'+searchResults[0].name+' & brawl is: '
-            +searchResults[0].legalities.brawl+'</div><img src="'
-            +searchResults[0].image_uris.small+'">';
-        document.getElementById("demo").innerHTML = output;
-        console.log(searchResults[0]);
+    /*fetch('https://api.scryfall.com/cards/search?order=cmc&q=' + searchText)
+     need to change things for this to work again (needs to take a array not single card)*/
+    var url = 'https://api.scryfall.com/cards/multiverse/' + searchText ;
+    document.getElementById("demo").innerHTML = url;
+
+    fetch(url)
+    .then(res => { return res.json() })
+    .then(data => {
         
+        console.log(data);
+      
+        let output = '<form name="myForm" action="./ScryfallScrapper.php" method="post"><input name="name" type="text" value="'
+            + data.name
+            + '"><input type="text" name="cmc" value="' + data.mana_cost
+            + '"><input type="text" name="cardType" value="' + data.type_line
+            + '"><input type="text" name="color" value="' + data.colors
+            + '"><input type="text" name="legBrawl" value="' + data.legalities.brawl
+            + '"><input type="text" name="legCommander" value="' + data.legalities.commander
+            + '"><input type="text" name="legDuel" value="' + data.legalities.duel
+            + '"><input type="text" name="legFuture" value="' + data.legalities.future
+            + '"><input type="text" name="legHistoric" value="' + data.legalities.historic
+            + '"><input type="text" name="legLegacy" value="' + data.legalities.legacy
+            + '"><input type="text" name="legModern" value="' + data.legalities.modern
+            + '"><input type="text" name="legOlderschool" value="' + data.legalities.oldschool
+            + '"><input type="text" name="legPauper" value="' + data.legalities.pauper
+            + '"><input type="text" name="legPenny" value="' + data.legalities.penny
+            + '"><input type="text" name="legPioneer" value="' + data.legalities.pioneer
+            + '"><input type="text" name="legStandard" value="' + data.legalities.standard
+            + '"><input type="text" name="legVintage" value="' + data.legalities.vintage
+            + '"><input type="text" name="power" value="' + data.power
+            + '"><input type="text" name="rarity" value="' + data.rarity
+            + '"><input type="text" name="setName" value="' + data.set_name
+            + '"><input type="text" name="toughness" value="' + data.toughness
+            + '"><input type="text" name="image" value="' + data.image_uris.large
+            + '"><input type="submit" name="submit" value="Submit">'
+            + '</form > '
+            + '<image src="' + data.image_uris.large + '">';
+            + '<?php$ (document).ready(function(){$("#myForm").submit();}); ?>'
+        document.getElementById("demo").innerHTML = output;        
     });
 }
---
+++ term/ScryfallScrapper.php
@@ -0,0 +1,88 @@
+<!
+Daniel Louis
+Internet Systems Programing - Fall 2019
+Term project: card deck database for magic the gatehring 
+>
+<!DOCTYPE html>
+<html><head>
+<title>Scraper</title>
+</head>
+<body>
+<?php
+	$submit = $_POST["submit"];
+	$name = $_POST["name"];
+	$cmc = $_POST["cmc"];
+	$cardType = $_POST["cardType"];
+	$color = $_POST["color"];
+	$legBrawl = $_POST["legBrawl"];
+	$legCommander = $_POST["legCommander"];
+	$legDuel = $_POST["legDuel"];
+	$legFuture = $_POST["legFuture"];
+	$legHistoric = $_POST["legHistoric"];
+	$legLegacy = $_POST["legLegacy"];
+	$legModern = $_POST["legModern"];
+	$legOlderschool = $_POST["legOlderschool"];
+	$legPauper = $_POST["legPauper"];
+	$legPenny = $_POST["legPenny"];
+	$legPioneer = $_POST["legPioneer"];
+	$legStandard = $_POST["legStandard"];
+	$legVintage = $_POST["legVintage"];
+	$power = $_POST["power"];
+	$rarity = $_POST["rarity"];
+	$setName = $_POST["setName"];
+	$toughness = $_POST["toughness"];
+	$image = $_POST["image"];
+	session_start();
+	$_SESSION['number'] = $_SESSION['number'] + 1;
+
+
+	if($power == "undefined")
+	{
+		$power = null;
+		$toughness = null;
+	}
+	else
+	{
+		$power = (int)$power;
+		$toughness = (int)$toughness;
+	}
+
+	$db = new mysqli("db1.cs.uakron.edu:3306", "dtl29", "Pah8quei", "ISP_dtl29");		
+	if ($db->connect_error) {
+		print "Error - Could not connect to MySQL";
+		exit;
+	}
+	$stmt = $db->prepare("INSERT INTO Cards(name, cmc, cardType, color,
+legBrawl,legCommander, legDuel, legFuture, legHistoric, legLegacy, 
+legModern,legOlderschool, legPauper, legPenny, legPioneer, 
+legStandard, legVintage,
+power, rarity, setName, toughness, image)
+VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+	$stmt->bind_param("sssssssssssssssssissis",$name,$cmc,$cardType,$color,$legBrawl,$legCommander,$legDuel,
+	$legFuture,$legHistoric,$legLegacy,$legModern,$legOlderschool,$legPauper,$legPenny,$legPioneer,$legStandard,
+	$legVintage,$power,$rarity,$setName,$toughness,$image);
+	
+	if($stmt->execute())
+	{
+		echo '<div>Entered into database correctly </div>';
+		console.log(mysqli_stmt_error( $stmt ));
+	}
+	else
+	{
+		echo '<div>The entery failed. </div>';
+		console.log(mysqli_stmt_error( $stmt ));
+	}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <br/>
+        <script src="ScryfallScapper.js"></script>
+    </head>
+    <body onload="scrap('<?php print($_SESSION['number']) ?>')">
+        <div id="demo"></div>
+    </body>
+</html>
+</body>
+</html>
